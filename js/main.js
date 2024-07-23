b0510ab89faa0bdd3857309fd9963faed247fa4f
Aldanaramos <aldiiramos2003@gmail.com>
2024-07-22 22:43:33 -0300

    elimine la carpeta new file

--- js/main.js
+++ js/main.js
@@ -4,6 +4,26 @@
             tasaInteres = tasaInteres / 100 ;
             //calcular el monto total del prestamo
             montoTotal = montoPrestamo * (1 + tasaInteres * plazo)
-                return montoTotal
+            let monto = 10000; // monto del prestamo
+            let tasaInteres = 0.05; // tasa de interes
+            let plazo = 5; // plazo del prestamo en años
+            console.log ("Monto del prestamo:" + monto);
+            return montoTotal
         }
+
+
+            // calculamos el monto mensual a pagar 
+            function CalcularMontoPagos(montoPrestamo, tasaInteres, plazo) {
+                let montoMensual = monto * (tasaInteres / 12) / 1 - (1 + tasaInteres / 12 ) * (-plazo * 12);
+            console.log("El monto mensual a pagar es:"+ montoMensual);
+            return montoMensual
+            }
+
+            
+
+                
+        
+
+
+
 
